102b88c76a356df05802375bdbfde8c249768bd0
Ludovic Maître <ludovic.maitre@effervens.com>
2017-09-05 11:20:52 +0200

    Cleanup

--- library/bonita.py
+++ library/bonita.py
@@ -24,32 +24,16 @@ short_description: Module to manage Bonita platform from Ansible
 '''
 
 EXAMPLES = '''
-- name: Login to Bonita
-  bonita:
-    session: login
-    url: "{{ bonita_url }}"
-    username: "{{bonita_credentials.username}}"
-    password: "{{bonita_credentials.passwod}}"
-- name: Get session
-  bonita:
-    session: get
-  register: bonita_session
-- name: Display session
-  debug: msg="{{ bonita_session }}"
-- name: Login to platform and use alternate configuration location
-  bonita:
-    platform: login
-    url: "{{ bonita_url }}"
-    username: "{{bonita_platform_credentials.username}}"
-    password: "{{bonita_platform_credentials.passwod}}"
-    configuration: "{{ playbook_dir + '/.bonita' }}"    
-- name: Stop platform
-  bonita:
-    platform: stop
-  register: stop_result_code
-- name: Get platform
-  bonita:
-    platform: get
+  - name: Login to Bonita
+    # bonita session [login <url> <username> <password>|logout|get]
+    bonita:
+      args: 
+        session: yes
+        login: yes
+        "<url>": "{{ bonita_url }}"
+        "<username>": "{{ bonita_credentials.login }}"
+        "<password>": "{{ bonita_credentials.password }}"
+      verbose: yes
 ...
 '''
 
@@ -62,9 +46,8 @@ class Bonita:
     Class used to centralize the operations with Bonita
     """
 
-    def __init__(self, options, verbose=False):
+    def __init__(self, options):
         self.options = options
-        self.logs = []
 
     def run(self):
         for (k, v) in self.options.items(): 
@@ -73,12 +56,9 @@ class Bonita:
                 bonita.commands = getmembers(module, isclass)
                 command = [command[1] for command in bonita.commands if command[0] != 'Base' and getmodule(command[1]).__name__.startswith('bonita.commands') ][0]
                 command = command(self.options)
+                command.headless = True
                 command.run()
-
-    def log(self, s):
-        self.logs.append(s)
-        logging.getLogger(LOGGER_NAME).debug(s)
-
+                return command.results
 
 def main():
     module = AnsibleModule(
@@ -94,26 +74,15 @@ def main():
         logging.basicConfig(filename=LOGGER_FILE,
                             level=logging.DEBUG)
         LOGGER.debug('--- %s ---', time.strftime("%H:%M:%S"))
-        LOGGER.debug(sys.path)
+        #LOGGER.debug(sys.path)
     else:
         LOGGER.setLevel(logging.INFO)
 
-    LOGGER.debug('Bonita cli available: %s' % HAS_BONITA_CLI)
+    #LOGGER.debug('Bonita cli available: %s' % HAS_BONITA_CLI)
     bonita_cli = Bonita(module.params['args'])
-    bonita_cli.run()
-
-    result = dict() 
-    result['rc'] = 200
-
-    fact = None
-    changed = True
-    if fact is not None:
-        ansible_facts = dict()
-        ansible_facts[fact] = result
-        module.exit_json(changed=changed, result=result,
-                         ansible_facts=ansible_facts)
-    else:
-        module.exit_json(changed=changed, result=result)
+    result = bonita_cli.run()
+    
+    module.exit_json(changed=changed, result=result)
 
 if __name__ == '__main__':
     main()
